32f8a3a27f48f1118bcda1b83b0ac75729c6e91e
Mikhail <mikhail.erastov@gmail.com>
2025-12-24 23:42:43 +0500

    Learn list

--- Base/list.py
+++ Base/list.py
@@ -33,3 +33,26 @@ print(numbers[:5])
 print(numbers[2:5])
 print(numbers[1:7:2])
 print(numbers[::2])
+
+numbers3 = numbers[2:6]
+numbers4 = numbers3[:]
+print(numbers3)
+print(id(numbers3), id(numbers4))
+
+big_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_big_numbers = big_numbers[:]
+print(new_big_numbers)
+del new_big_numbers[::2]
+print(new_big_numbers)
+new_big_numbers[2:6:2] = [100, 200]
+print(new_big_numbers)
+
+# В extend передается только итерируемый объект
+list_names = ["Michael", "Bob", "Jim"]
+list_names.extend("abc")
+print(list_names)
+# # Должна быть ошибка TypeError
+# list_names.extend(True)
+# print(list_names)
+
+print("<>".join("list_names"))
